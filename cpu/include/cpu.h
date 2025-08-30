3600e4230b59970c6d10ef9e1a18ffa9790de570
JavierG <javgilleo@alum.us.es>
2025-08-30 20:09:36 +0200

    [DMG-5] Changed the way to access F reg

--- cpu/include/cpu.h
+++ cpu/include/cpu.h
@@ -4,6 +4,11 @@
 
 #include "common.h"
 
+#define Z_FLAG 0x80 // 1000 0000b
+#define N_FLAG 0x40 // 0100 0000b
+#define H_FLAG 0x20 // 0010 0000b
+#define C_FLAG 0x10 // 0001 0000b
+										
 typedef union cpu_regs{
 	struct{
 		byte High;
@@ -12,37 +17,13 @@ typedef union cpu_regs{
 	word all;
 }CPU_REGS;
 
-typedef struct flags{
-	unsigned unused: 4;
-	unsigned C     : 1;
-	unsigned H     : 1;
-	unsigned N     : 1;
-	unsigned Z     : 1;
-}FLAGS;
-
-
-typedef union reg_F{
-	FLAGS bit;
-	byte  all;
-}REG_F;
-
-typedef union cpu_acc_flags{
-	word all;
-
-	struct{
-		byte High; 				//Accumulator
-	  REG_F Low;
-	};
-
-}CPU_ACC_FLAGS;
-
 typedef struct gameboy_cpu{
-	CPU_ACC_FLAGS  AF;   // Accumulator & Flags    
-	CPU_REGS			 BC;   // Registers B & C
-	CPU_REGS			 DE;   // Registers D & E
-	CPU_REGS			 HL;   // Registers H & L
-	word					 SP;   // Stack Pointer
-	word					 PC;   // Program Counter
+	CPU_REGS       AF;   // Accumulator & Flags    
+	CPU_REGS	     BC;   // Registers B & C
+	CPU_REGS	     DE;   // Registers D & E
+	CPU_REGS	     HL;   // Registers H & L
+	word		       SP;   // Stack Pointer
+	word		       PC;   // Program Counter
 } GAMEBOY_CPU;
 
 
